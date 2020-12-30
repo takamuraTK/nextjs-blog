59a505301fc78ec789aef85cc955d459d10cd9c7
takamuraTK <tirpitz.zzz@outlook.jp>
2020-12-30 14:56:33 +0900

    fix id type assertion

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -31,7 +31,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id);
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData,
